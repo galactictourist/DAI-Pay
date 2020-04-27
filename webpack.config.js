95bc0980a5fcdf9f10133362fa4069187acb9e0f
galactictourist <galactictourist@gmail.com>
2020-04-27 14:49:12 -0700

    procfile edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
-  entry: "./client/index.js",
+  mode: "production",
+  entry: "client/index.js",
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js", // string
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    compress: true,
-    port: 8080,
   },
 };
