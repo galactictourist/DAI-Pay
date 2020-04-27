2c74255fcf0094f88acf09ce5167835928c5cf38
galactictourist <galactictourist@gmail.com>
2020-04-27 15:40:26 -0700

    setup heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "client/index.js",
+  entry: path.resolve(__dirname + "/client/index.js"),
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js", // string
