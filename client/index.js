410261d2628c5dfa038ad5702507c28d125dc1be
galactictourist <galactictourist@gmail.com>
2020-05-16 23:51:56 -0700

    remove metamask

--- client/index.js
+++ client/index.js
@@ -4,22 +4,29 @@ const libZ = require("../lib");
 
 const initWeb3 = () => {
   return new Promise((resolve, reject) => {
-    if (typeof window.ethereum !== "undefined") {
-      const web3 = new Web3(window.ethereum);
-      window.ethereum
-        .enable()
-        .then(() => {
-          resolve(new Web3(window.ethereum));
-        })
-        .catch((e) => {
-          reject(e);
-        });
-      return;
-    }
-    if (typeof window.web3 !== "undefined") {
-      return resolve(new Web3(window.web3.currentProvider));
-    }
-    resolve(new Web3("http://localhost:9545"));
+    // if (typeof window.ethereum !== "undefined") {
+    //   const web3 = new Web3(window.ethereum);
+    //   window.ethereum
+    //     .enable()
+    //     .then(() => {
+    //       resolve(new Web3(window.ethereum));
+    //     })
+    //     .catch((e) => {
+    //       reject(e);
+    //     });
+    //   return;
+    // }
+    // if (typeof window.web3 !== "undefined") {
+    //   return resolve(new Web3(window.web3.currentProvider));
+    // }
+
+    //*****Test Code - REMOVE */
+
+    return resolve(new Web3(window.web3.currentProvider));
+
+    //****END TEST CODE */
+
+    // resolve(new Web3("http://localhost:9545"));
   });
 };
 
